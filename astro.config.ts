1bb0c89587a0b633b8af76d39364a8ecfb8e9eb0
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-14 21:38:42 +0200

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,13 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://astro-deno-deploy.deno.dev/",
-	integrations: [sitemap(), critters({ logger: 1 }), prefetch(), rome({ logger: 1 }), compress({ logger: 1 })],
+	integrations: [
+		sitemap(),
+		critters({ logger: 1 }),
+		prefetch(),
+		rome({ logger: 1 }),
+		compress({ logger: 1 }),
+	],
 	output: "server",
 	adapter: deno(),
 });
