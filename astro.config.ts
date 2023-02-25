ac093d26235b1b679f026bae358a071f1df5db74
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-25 03:39:04 +0200

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
 // @ts-nocheck
 import { defineConfig } from "astro/config";
 
-import compress from "astro-compress";
-import critters from "astro-critters";
 import deno from "@astrojs/deno";
 import prefetch from "@astrojs/prefetch";
-import rome from "astro-rome";
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import critters from "astro-critters";
+import rome from "astro-rome";
 import worker from "astrojs-service-worker";
 
 export default defineConfig({
