9fdf3763001ede0d5539acbf5931b0e8be223be5
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-04 20:07:28 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import worker from "astrojs-service-worker";
 export default defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
+	outDir: "./Build",
 	site: "https://astro-deno-deploy.deno.dev",
 	experimental: {
 		assets: true,
