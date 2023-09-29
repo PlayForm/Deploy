8a80b77371a4c9a90829083f0c153afa5d78b6f6
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-29 14:10:59 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import deno from "@astrojs/deno";
 
-=======
->>>>>>> upstream/main
 export default (await import("astro/config")).defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
@@ -10,23 +7,14 @@ export default (await import("astro/config")).defineConfig({
 	site: "https://astro-deno-deploy.deno.dev",
 	compressHTML: true,
 	integrations: [
-<<<<<<< HEAD
-		import.meta.env.MODE === "production" ? (await import("astrojs-service-worker")).default() : null,
-		(await import("@astrojs/sitemap")).default(),
-		(await import('astro-critters')).default({ Logger: 1 }),
-		(await import('@astrojs/prefetch')).default(),
-		(await import('astro-rome')).default({ Logger: 1 }),
-		(await import('astro-compress')).default({ Logger: 1 }),
-=======
 		import.meta.env.MODE === "production"
-		? (await import("astrojs-service-worker")).default()
-		: null,
+			? (await import("astrojs-service-worker")).default()
+			: null,
 		(await import("@astrojs/sitemap")).default(),
 		(await import("astro-critters")).default({ Logger: 1 }),
 		(await import("@astrojs/prefetch")).default(),
 		(await import("astro-rome")).default({ Logger: 1 }),
 		(await import("astro-compress")).default({ Logger: 1 }),
->>>>>>> upstream/main
 	],
 	output: "server",
 	adapter: deno(),
